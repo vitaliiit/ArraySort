a3c1b22fa4ebc8d2e9c615a03b6085438770f97f
Vitaliiit <vv8307386@gmail.com>
2020-09-26 21:57:24 +0300

    update

--- src/ArrayE.java
+++ src/ArrayE.java
@@ -1,22 +1,25 @@
 public class ArrayE {
 
-    public void sort(int[] array) {
-        for (int j = 0; j < array.length; j++) {
+    public void sort(int[] array)
+    {
+        for (int j = 0; j < array.length; j++)
+        {
             runInnerCycle(array);
         }
     }
 
-    private void runInnerCycle(int[] array) {
+    private void runInnerCycle(int[] array)
+    {
+        for (int i = 1; i < array.length; i++)
+        {
 
-        for (int i = 1; i < array.length; i++) {
+            boolean leftGreaterThanRight = array[i - 1] > array[i]; // то просто принцип? - array[i - 1] > array[i] - який ніби говорить якщо число поперднього індексу більше за число наступного індексу - вважати за тру!
 
-            boolean leftGreaterThanRight = array[i - 1] > array[i];
-
-            if (leftGreaterThanRight == true) { // чому тут можна без '== true'
-
-                int z = array[i];
-                array[i] = array[i - 1];
-                array[i - 1] = z;
+            if (leftGreaterThanRight)
+            {
+                int z = array[i]; // то просто - число, яке знаходиться під наступним індексом - присвоюється полю z (буфер обміну)
+                array[i] = array[i - 1]; // тут - наступному індексу присвоюється значення попереднього індексу
+                array[i - 1] = z; // тут - попердньому індексу присвоюється значення наступного індексу котре було збережене в змінній z (буфері обміну)
             }
         }
     }
--- src/Central.java
+++ src/Central.java
@@ -1,22 +1,26 @@
-public class Central {
-    public static void main(String[] args) {
+import java.util.Arrays;
+
+public class Central
+{
+    public static void main(String[] args)
+    {
         int[] array = new int[10];
+
         array[0] = 9;
-        array[1] = 8;
-        array[2] = 7;
         array[3] = 6;
-        array[4] = 5;
-        array[5] = 4;
-        array[6] = 3;
-        array[7] = 2;
-        array[8] = 1;
+        array[1] = 8;
         array[9] = 0;
+        array[2] = 7;
+        array[5] = 4;
+        array[8] = 1;
+        array[6] = 3;
+        array[4] = 5;
+        array[7] = 2;
 
         ArrayE arrayE = new ArrayE();
         arrayE.sort(array);
 
-        for ( int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
+            System.out.println(Arrays.toString(array));
+
     }
 }
